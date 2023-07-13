e38a048f0be0c47d24a2b6b0b174979e76fa4cd4
zzdroide <196064264+zzdroide@users.noreply.github.com>
2023-07-13 00:00:53 -0400

    feat: basic logging server

---
+++ src/server.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+import socketserver
+
+import paramiko
+
+@dataclass
+class MyServer(paramiko.ServerInterface):
+    client_ip: str
+    username_printed = False
+
+    def get_banner(self):
+        # return ("\ndo not type your password here\n\n", "en-US")
+        return ("\n  ಠ_ಠ\n\n", "en-US")
+
+    def get_allowed_auths(self, username):
+        if not self.username_printed:
+            print(f"{self.client_ip}\tuser\t{username}")
+            self.username_printed = True
+
+        return "password,publickey"
+
+    def check_auth_password(self, username, password):
+        print(f"{self.client_ip}\tpass\t{username}:{password}")
+        # TODO: save
+        return paramiko.AUTH_FAILED
+
+    def check_auth_publickey(self, username, key):
+        print(f"{self.client_ip}\tpub\t{username}:{key.fingerprint}")
+        return paramiko.AUTH_FAILED
+
+    def check_channel_request(self, kind, chanid):
+        # Client can't open a channel without authenticating
+        raise AssertionError("never reached")
+
+
+class MyTransport(paramiko.Transport):
+    # @static
+    host_key = paramiko.RSAKey.generate(bits=2048)
+
+    def __init__(self, *args, **kwargs):
+        # TODO: get latest version automatically from https://pypi.org/project/apt-repo/
+        self._CLIENT_ID = "OpenSSH_8.2p1 Ubuntu-4ubuntu0.7"
+        super().__init__(*args, **kwargs)
+        self.add_server_key(MyTransport.host_key)
+
+
+class ReqHandler(socketserver.BaseRequestHandler):
+    client_ip: str
+    my_server: MyServer
+    transport: MyTransport
+
+    def setup(self):
+        self.client_ip = self.client_address[0]
+        print(f"{self.client_ip}\tconnected")
+        self.my_server = MyServer(self.client_ip)
+        self.transport = MyTransport(self.request)
+
+    def handle(self):
+        try:
+            self.transport.start_server(server=self.my_server)
+            chan = self.transport.accept(30)
+            assert chan is None     # Client can't open a channel without authenticating
+        except (OSError, EOFError):
+            pass
+
+    def finish(self):
+        self.transport.close()
+        print(f"{self.client_ip}\tdisconnected")
+
+
+PORT = 2222
+
+with socketserver.ThreadingTCPServer(('0.0.0.0', PORT), ReqHandler) as server:
+    print(f"Listening on port {PORT}")
+    server.serve_forever()
