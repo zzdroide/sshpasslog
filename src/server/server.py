251332c4e8ec5198174b50ea02284db838f20b3d
zzdroide <196064264+zzdroide@users.noreply.github.com>
2023-07-15 22:35:54 -0400

    refactor: username_printed

--- src/server/server.py
+++ src/server/server.py
@@ -14,30 +14,43 @@ class MyServer(paramiko.ServerInterface, LoggingMixin):
     client_ip_addr: str
     client_ip_country: str
 
+    username_printed: bool
+
     def __init__(self, src: 'ReqHandler'):
         self.client_ip_addr = src.client_ip_addr
         self.client_ip_country = src.client_ip_country
 
-    username_printed = False
+        self.username_printed = False
 
     def get_banner(self):
         # return ("\ndo not type your password here\n\n", "en-US")
         return ("\n  ಠ_ಠ\n\n", "en-US")
 
-    def get_allowed_auths(self, username):
+    def on_got_username(self, username: str):
+        """
+        This makes possible to print the bare username before a password is sent.
+        """
         if not self.username_printed:
             self.log('user', username)
             self.username_printed = True
 
+    def get_allowed_auths(self, username):
+        self.on_got_username(username)
         return "password,publickey"
+
+    def check_auth_none(self, username):
+        self.on_got_username(username)
+        return paramiko.AUTH_FAILED
 
     def check_auth_password(self, username, password):
         self.log('pass', f'{username}:{password}')
+        self.username_printed = True
         db.record_pass(username, password, self.client_ip_addr, self.client_ip_country)
         return paramiko.AUTH_FAILED
 
     def check_auth_publickey(self, username, key):
         self.log('pub', f'{username} {key.fingerprint}')
+        self.username_printed = True
         return paramiko.AUTH_FAILED
 
     def check_channel_request(self, kind, chanid):
