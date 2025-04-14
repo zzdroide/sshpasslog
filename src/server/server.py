6ce9b33be01e23faf7691397027ad21da3d4305d
zzdroide <196064264+zzdroide@users.noreply.github.com>
2025-04-14 18:16:03 -0400

    fix: Address already in use

--- src/server/server.py
+++ src/server/server.py
@@ -12,7 +12,7 @@ from src.log import LoggingMixin, log_exceptions, logger
 VERSION_STR_PREFIX_LEN = len("SSH-2.0-")
 
 
-class MyServer(paramiko.ServerInterface, LoggingMixin):
+class MySshServer(paramiko.ServerInterface, LoggingMixin):
     client_ip_addr: str
     client_ip_country: str
 
@@ -104,20 +104,20 @@ class ReqHandler(socketserver.BaseRequestHandler, LoggingMixin):
     client_ip_addr: str
     client_ip_country: str
 
-    my_server: MyServer
+    ssh_server: MySshServer
     transport: MyTransport
 
     def setup(self):
         self.client_ip_addr = self.client_address[0]
         self.client_ip_country = ip2country(self.client_ip_addr)
         self.log("conn")
-        self.my_server = MyServer(self)
+        self.ssh_server = MySshServer(self)
         self.transport = MyTransport(self.request)
 
     def handle(self):
         try:
             event = threading.Event()
-            self.transport.start_server(event=event, server=self.my_server)
+            self.transport.start_server(event=event, server=self.ssh_server)
             event.wait(ReqHandler.NEG_TIMEOUT)
 
             if self.transport.remote_version:
@@ -134,10 +134,14 @@ class ReqHandler(socketserver.BaseRequestHandler, LoggingMixin):
         self.log("disc")
 
 
+class MyTcpServer(socketserver.ThreadingTCPServer):
+    allow_reuse_address = True
+
+
 PORT = 2222
 
 
 def run():
-    with socketserver.ThreadingTCPServer(("0.0.0.0", PORT), ReqHandler) as server:  # noqa: S104
+    with MyTcpServer(("0.0.0.0", PORT), ReqHandler) as server:  # noqa: S104
         logger.info(f"Listening on port {PORT}")
         server.serve_forever()
